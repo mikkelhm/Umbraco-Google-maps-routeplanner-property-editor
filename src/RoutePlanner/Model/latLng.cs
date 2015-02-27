07e2ac79578d4b48c4f337fe23216aa67d7388ea
Rasmus Fjord <rasmusfjord@gmail.com>
2015-02-27 09:58:56 +0100

    Added kml import, and a serialiable model

---
+++ src/RoutePlanner/Model/latLng.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutePlanner.Model
+{
+   public class latLng
+    {
+        public string k { get; set; }
+        public string B { get; set; }
+    }
+}
